699575a40e2cfc5846c1966cfaa33f784190b0f1
YarinceP <padilla.yarince@gmail.com>
2021-03-18 00:06:08 -0600

    remove unused package

--- tests/Feature/MovieManagementTest.php
+++ tests/Feature/MovieManagementTest.php
@@ -3,8 +3,6 @@
 namespace Tests\Feature;
 
 use GuzzleHttp\Promise\PromiseInterface;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
