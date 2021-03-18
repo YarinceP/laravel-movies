004c94a3319420cd797637aaae0ec5f2a14bf3b6
YarinceP <padilla.yarince@gmail.com>
2021-03-18 00:04:13 -0600

    the main page shows correct info

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class MovieController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $popular_movies = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/movie/popular')
+            ->json()['results'];
+
+        $genresArray = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/genre/movie/list')
+            ->json()['genres'];
+
+        $genres = collect($genresArray)->mapWithKeys(function ($genre) {
+            return [$genre['id'] => $genre['name']];
+        });
+        return view('index', compact('popular_movies','genres'));
     }
 
     public function show()
--- tests/Feature/MovieManagementTest.php
+++ tests/Feature/MovieManagementTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Feature;
 
+use GuzzleHttp\Promise\PromiseInterface;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class MovieManagementTest extends TestCase
@@ -18,6 +20,7 @@ class MovieManagementTest extends TestCase
         $response->assertStatus(200);
         $response->assertViewIs('index');
     }
+
     /**
      * @test
      */
@@ -28,5 +31,138 @@ class MovieManagementTest extends TestCase
 
         $response->assertStatus(200);
         $response->assertViewIs('show');
+    }
+
+    /**
+     * @test
+     */
+    public function the_main_page_shows_correct_info()
+    {
+        Http::fake([
+            'https://api.themoviedb.org/3/movie/popular' => $this->fakePopularMovies(),
+            'https://api.themoviedb.org/3/genre/movie/list' => $this->fakeGenres()
+        ]);
+
+        $response = $this->get('/');
+
+        $this->withoutExceptionHandling();
+        $response->assertSuccessful();
+        $response->assertSee('Popular Movies');
+        $response->assertSee('Fake Movie');
+        $response->assertSee('Adventure');
+       //$response->assertSee('Now Playing');
+        //$response->assertSee('Now Playing Fake Movie');
+    }
+
+
+    private function fakePopularMovies(): PromiseInterface
+    {
+        return Http::response([
+            'results' => [
+                [
+                    "popularity" => 406.677,
+                    "vote_count" => 2607,
+                    "video" => false,
+                    "poster_path" => "/xBHvZcjRiWyobQ9kxBhO6B2dtRI.jpg",
+                    "id" => 419704,
+                    "adult" => false,
+                    "backdrop_path" => "/5BwqwxMEjeFtdknRV792Svo0K1v.jpg",
+                    "original_language" => "en",
+                    "original_title" => "Fake Movie",
+                    "genre_ids" => [
+                        12,
+
+                    ],
+                    "title" => "Fake Movie",
+                    "vote_average" => 6,
+                    "overview" => "Fake movie description. The near future, a time when both hope and hardships drive humanity to look to the stars and beyond. While a mysterious phenomenon menaces to destroy life on planet earth.",
+                    "release_date" => "2019-09-17",
+                ]
+            ]
+        ], 200);
+    }
+
+    private function fakeGenres(): PromiseInterface
+    {
+        return Http::response([
+            'genres' => [
+                [
+                    "id" => 28,
+                    "name" => "Action"
+                ],
+                [
+                    "id" => 12,
+                    "name" => "Adventure"
+                ],
+                [
+                    "id" => 16,
+                    "name" => "Animation"
+                ],
+                [
+                    "id" => 35,
+                    "name" => "Comedy"
+                ],
+                [
+                    "id" => 80,
+                    "name" => "Crime"
+                ],
+                [
+                    "id" => 99,
+                    "name" => "Documentary"
+                ],
+                [
+                    "id" => 18,
+                    "name" => "Drama"
+                ],
+                [
+                    "id" => 10751,
+                    "name" => "Family"
+                ],
+                [
+                    "id" => 14,
+                    "name" => "Fantasy"
+                ],
+                [
+                    "id" => 36,
+                    "name" => "History"
+                ],
+                [
+                    "id" => 27,
+                    "name" => "Horror"
+                ],
+                [
+                    "id" => 10402,
+                    "name" => "Music"
+                ],
+                [
+                    "id" => 9648,
+                    "name" => "Mystery"
+                ],
+                [
+                    "id" => 10749,
+                    "name" => "Romance"
+                ],
+                [
+                    "id" => 878,
+                    "name" => "Science Fiction"
+                ],
+                [
+                    "id" => 10770,
+                    "name" => "TV Movie"
+                ],
+                [
+                    "id" => 53,
+                    "name" => "Thriller"
+                ],
+                [
+                    "id" => 10752,
+                    "name" => "War"
+                ],
+                [
+                    "id" => 37,
+                    "name" => "Western"
+                ],
+            ]
+        ], 200);
     }
 }
