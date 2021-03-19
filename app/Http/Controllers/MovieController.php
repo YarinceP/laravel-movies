af0fbc033fa06f86c9870639764e8dc3137afdff
YarinceP <padilla.yarince@gmail.com>
2021-03-19 15:37:01 -0600

    endpoint in show method

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -23,11 +23,15 @@ class MovieController extends Controller
         $genres = collect($genresArray)->mapWithKeys(function ($genre) {
             return [$genre['id'] => $genre['name']];
         });
-        return view('index', compact('popularMovies','genres','nowPLayingMovies'));
+        return view('index', compact('popularMovies', 'genres', 'nowPLayingMovies'));
     }
 
-    public function show()
+    public function show($id)
     {
-        return view('show');
+        $movie = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/movie/' . $id . '?append_to_response=credits,videos,images')
+            ->json();
+
+        return view('show', compact('movie'));
     }
 }
