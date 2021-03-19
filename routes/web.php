fbf95f10f5dba5f9ec81432ad8d83de8447e0a19
YarinceP <padilla.yarince@gmail.com>
2021-03-19 15:35:53 -0600

    name routes and add id param

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[MovieController::class,'index']);
-Route::get('/movie',[MovieController::class,'show']);
+Route::get('/', [MovieController::class, 'index']);
+Route::get('/movies/{id}', [MovieController::class, 'show'])->name('movies.show');
