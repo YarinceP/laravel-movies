7544998b5c559aac7451b700b3b1378348ffc846
YarinceP <padilla.yarince@gmail.com>
2021-03-19 15:33:00 -0600

    testing API

--- tests/Feature/MovieManagementTest.php
+++ tests/Feature/MovieManagementTest.php
@@ -22,18 +22,6 @@ class MovieManagementTest extends TestCase
     /**
      * @test
      */
-    public function show_view_can_be_rendered()
-    {
-        $this->withoutExceptionHandling();
-        $response = $this->get('/movie');
-
-        $response->assertStatus(200);
-        $response->assertViewIs('show');
-    }
-
-    /**
-     * @test
-     */
     public function the_main_page_shows_correct_info()
     {
         Http::fake([
@@ -51,6 +39,37 @@ class MovieManagementTest extends TestCase
         $response->assertSee('Adventure');
         $response->assertSee('Now Playing');
         $response->assertSee('Now Playing Fake Movie');
+    }
+
+    /** @test */
+    public function the_movie_page_shows_the_correct_info()
+    {
+        Http::fake([
+            'https://api.themoviedb.org/3/movie/*' => $this->fakeSingleMovie(),
+        ]);
+
+        $response = $this->get(route('movies.show', 12345));
+
+        //Title
+        $response->assertSee('Fake Jumanji: The Next Level');
+        //Vote average
+        $response->assertSee('68%');
+        //Release date
+        $response->assertSee('2019-12-04');
+        //Genres
+        $response->assertSee('Action, Adventure, Comedy, Fantasy');
+        //Sinopsis
+        $response->assertSee('As the gang return to Jumanji to rescue one of their own, they discover that nothing is as they expect. The players will have to brave parts unknown and unexplored.');
+        //Crew section
+        $response->assertSee('Jeanne McCarthy');
+        $response->assertSee('Casting Director');
+        //Button play trailer
+        $response->assertSee('Play Trailer');
+        //Cast section
+        $response->assertSee('Dwayne Johnson');
+        $response->assertSee('Dr. Smolder Bravestone');
+        //Images section
+        $response->assertSee('Images');
     }
 
 
@@ -196,6 +215,88 @@ class MovieManagementTest extends TestCase
                     "vote_average" => 6,
                     "overview" => "Now playing fake movie description. The near future, a time when both hope and hardships drive humanity to look to the stars and beyond. While a mysterious phenomenon menaces to destroy life on planet earth.",
                     "release_date" => "2019-09-17",
+                ]
+            ]
+        ], 200);
+    }
+
+    /**
+     * @return PromiseInterface
+     */
+    public function fakeSingleMovie(): PromiseInterface
+    {
+        return Http::response([
+            "adult" => false,
+            "backdrop_path" => "/hreiLoPysWG79TsyQgMzFKaOTF5.jpg",
+            "genres" => [
+                ["id" => 28, "name" => "Action"],
+                ["id" => 12, "name" => "Adventure"],
+                ["id" => 35, "name" => "Comedy"],
+                ["id" => 14, "name" => "Fantasy"],
+            ],
+            "homepage" => "http://jumanjimovie.com",
+            "id" => 12345,
+            "overview" => "As the gang return to Jumanji to rescue one of their own, they discover that nothing is as they expect. The players will have to brave parts unknown and unexplored.",
+            "poster_path" => "/bB42KDdfWkOvmzmYkmK58ZlCa9P.jpg",
+            "release_date" => "2019-12-04",
+            "runtime" => 123,
+            "title" => "Fake Jumanji: The Next Level",
+            "vote_average" => 6.8,
+            "credits" => [
+                "cast" => [
+                    [
+                        "cast_id" => 2,
+                        "character" => "Dr. Smolder Bravestone",
+                        "credit_id" => "5aac3960c3a36846ea005147",
+                        "gender" => 2,
+                        "id" => 18918,
+                        "name" => "Dwayne Johnson",
+                        "order" => 0,
+                        "profile_path" => "/kuqFzlYMc2IrsOyPznMd1FroeGq.jpg",
+                    ]
+                ],
+                "crew" => [
+                    [
+                        "credit_id" => "5d51d4ff18b75100174608d8",
+                        "department" => "Production",
+                        "gender" => 1,
+                        "id" => 546,
+                        "job" => "Casting Director",
+                        "name" => "Jeanne McCarthy",
+                        "profile_path" => null,
+                    ]
+                ]
+            ],
+            "videos" => [
+                "results" => [
+                    [
+                        "id" => "5d1a1a9b30aa3163c6c5fe57",
+                        "iso_639_1" => "en",
+                        "iso_3166_1" => "US",
+                        "key" => "rBxcF-r9Ibs",
+                        "name" => "JUMANJI: THE NEXT LEVEL - Official Trailer (HD)",
+                        "site" => "YouTube",
+                        "size" => 1080,
+                        "type" => "Trailer",
+                    ]
+                ]
+            ],
+            "images" => [
+                "backdrops" => [
+                    [
+                        "aspect_ratio" => 1.7777777777778,
+                        "file_path" => "/hreiLoPysWG79TsyQgMzFKaOTF5.jpg",
+                        "height" => 2160,
+                        "iso_639_1" => null,
+                        "vote_average" => 5.388,
+                        "vote_count" => 4,
+                        "width" => 3840,
+                    ]
+                ],
+                "posters" => [
+                    [
+
+                    ]
                 ]
             ]
         ], 200);
