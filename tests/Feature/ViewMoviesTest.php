f2206563ed26698b361ac4a13dae870a6abd0766
YarinceP <padilla.yarince@gmail.com>
2021-03-20 15:32:56 -0600

    search dropdown component flow

--- tests/Feature/ViewMoviesTest.php
+++ tests/Feature/ViewMoviesTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use GuzzleHttp\Promise\PromiseInterface;
 use Illuminate\Support\Facades\Http;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 class ViewMoviesTest extends TestCase
@@ -70,6 +71,23 @@ class ViewMoviesTest extends TestCase
         $response->assertSee('Dr. Smolder Bravestone');
         //Images section
         $response->assertSee('Images');
+    }
+
+
+    /**
+     * @test
+     */
+
+    public function the_search_dropdown_works_correctly()
+    {
+        $response = Http::fake([
+            'https://api.themoviedb.org/3/movie?query=jumanji' => $this->fakeSearchMovies(),
+        ]);
+
+        Livewire::test('search-dropdown')
+            ->assertDontSee('jumanji')
+            ->set('search', 'jumanji')
+            ->assertSee('Jumanji');
     }
 
 
@@ -297,6 +315,40 @@ class ViewMoviesTest extends TestCase
                     [
 
                     ]
+                ]
+            ]
+        ], 200);
+    }
+
+
+    /**
+     * @return PromiseInterface
+     */
+    private function fakeSearchMovies(): PromiseInterface
+    {
+        return Http::response([
+            'results' => [
+                [
+                    "popularity" => 406.677,
+                    "vote_count" => 2607,
+                    "video" => false,
+                    "poster_path" => "/xBHvZcjRiWyobQ9kxBhO6B2dtRI.jpg",
+                    "id" => 419704,
+                    "adult" => false,
+                    "backdrop_path" => "/5BwqwxMEjeFtdknRV792Svo0K1v.jpg",
+                    "original_language" => "en",
+                    "original_title" => "Jumanji",
+                    "genre_ids" => [
+                        12,
+                        18,
+                        9648,
+                        878,
+                        53,
+                    ],
+                    "title" => "Jumanji",
+                    "vote_average" => 6,
+                    "overview" => "Jumanji description. The near future, a time when both hope and hardships drive humanity to look to the stars and beyond. While a mysterious phenomenon menaces to destroy life on planet earth.",
+                    "release_date" => "2019-09-17",
                 ]
             ]
         ], 200);
